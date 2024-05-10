f4844ddb4f3a5a4dd1562d8e0fd2b546e835ce5b
dev <xorzero777@gmail.com>
2024-05-10 18:04:09 +0800

    harness: wrapped with tmux
    	todo: local controls & conf

--- harness.sh
+++ harness.sh
@@ -16,7 +16,7 @@ myip
 # Start
 ################################################################################
 
-set -ex
+set -evx
 
 export RPC_USER="user"
 export RPC_PASS="pass"
@@ -46,7 +46,8 @@ BILL_WALLET_PRIMARY_ADDRESS=
 
 # data
 NODES_ROOT=~/dextest/xmr
-WALLETS_DIR="${NODES_ROOT}/wallets"
+FRED_WALLET_DIR="${NODES_ROOT}/wallets/fred"
+BILL_WALLET_DIR="${NODES_ROOT}/wallets/bill"
 HARNESS_CTL_DIR="${NODES_ROOT}/harness-ctl"
 ALPHA_DATA_DIR="${NODES_ROOT}/alpha"
 ALPHA_REGTEST_CFG="${ALPHA_DATA_DIR}/alpha.conf"
@@ -54,7 +55,8 @@ ALPHA_REGTEST_CFG="${ALPHA_DATA_DIR}/alpha.conf"
 if [ -d "${NODES_ROOT}" ]; then
   rm -fR "${NODES_ROOT}"
 fi
-mkdir -p "${WALLETS_DIR}"
+mkdir -p "${FRED_WALLET_DIR}"
+mkdir -p "${BILL_WALLET_DIR}"
 mkdir -p "${HARNESS_CTL_DIR}"
 mkdir -p "${ALPHA_DATA_DIR}"
 touch    "${ALPHA_REGTEST_CFG}"           # currently empty
@@ -69,6 +71,20 @@ touch    "${ALPHA_REGTEST_CFG}"           # currently empty
 echo "Writing ctl scripts"
 
 echo "TODO:"
+
+# Shutdown script
+cat > "${NODES_ROOT}/harness-ctl/quit" <<EOF
+#!/usr/bin/env bash
+tmux send-keys -t $SESSION:3 C-c
+sleep 0.05
+tmux send-keys -t $SESSION:2 C-c
+sleep 0.05
+tmux send-keys -t $SESSION:1 C-c
+sleep 0.05
+# . . . 
+tmux kill-session
+EOF
+chmod +x "${HARNESS_CTL_DIR}/quit"
 
 ################################################################################
 # Configuration Files
@@ -87,28 +103,29 @@ SESSION="xmr-harness"
 # WAIT can be used in a send-keys call along with a `wait-for donexmr` command
 WAIT="; tmux wait-for -S donexmr"
 
-# tmux new-session -d -s $SESSION $SHELL
-# tmux rename-window -t $SESSION:0 'harness-ctl'
-# tmux send-keys -t $SESSION:0 "set +o history" C-m
-# tmux send-keys -t $SESSION:0 "cd ${HARNESS_CTL_DIR}" C-m
-
-echo "TODO:"
+tmux new-session -d -s $SESSION $SHELL
+tmux rename-window -t $SESSION:0 'harness-ctl'
+tmux send-keys -t $SESSION:0 "set +o history" C-m
+tmux send-keys -t $SESSION:0 "cd ${HARNESS_CTL_DIR}" C-m
 
 ################################################################################
 # SINGLE NODE
 ################################################################################
 
-monerod \
- --detach \
- "--pidfile=${ALPHA_DATA_DIR}/monerod.pid" \
-	--regtest \
-	--offline \
-   --data-dir "${ALPHA_DATA_DIR}" \
-   --config-file "${ALPHA_REGTEST_CFG}" \
+tmux new-window -t $SESSION:1 -n 'alpha' $SHELL
+tmux send-keys -t $SESSION:1 "set +o history" C-m
+tmux send-keys -t $SESSION:1 "cd ${ALPHA_DATA_DIR}" C-m
+
+# start alpha node
+tmux send-keys -t $SESSION:1 "monerod \
+   --regtest \
+   --offline \
+   --data-dir ${ALPHA_DATA_DIR} \
+   --config-file ${ALPHA_REGTEST_CFG} \
    --rpc-bind-ip 127.0.0.1 \
-   --rpc-bind-port "${ALPHA_NODE_RPC_PORT}" \
+   --rpc-bind-port ${ALPHA_NODE_RPC_PORT} \
    --fixed-difficulty 1 \
-   --log-level 1
+   --log-level 1; tmux wait-for -S alphaxmr" C-m
 
 sleep 3
 
@@ -116,27 +133,33 @@ sleep 3
 # WALLET CLIENTS
 ################################################################################
 
+tmux new-window -t $SESSION:2 -n 'fred' $SHELL
+tmux send-keys -t $SESSION:2 "set +o history" C-m
+tmux send-keys -t $SESSION:2 "cd ${FRED_WALLET_DIR}" C-m
+
 # Start the first wallet client
-monero-wallet-rpc \
---detach \
---pidfile="${WALLETS_DIR}/fred-wallet-rpc.pid" \
-	--rpc-bind-ip 127.0.0.1 \
-	--rpc-bind-port ${FRED_WALLET_RPC_PORT} \
-	--wallet-dir "${WALLETS_DIR}" \
-	--disable-rpc-login \
-    --allow-mismatched-daemon-version
+tmux send-keys -t $SESSION:2 "monero-wallet-rpc \
+   --rpc-bind-ip 127.0.0.1 \
+   --rpc-bind-port ${FRED_WALLET_RPC_PORT} \
+   --wallet-dir ${FRED_WALLET_DIR} \
+   --disable-rpc-login \
+   --allow-mismatched-daemon-version; tmux wait-for -S billxmr" C-m 
 sleep 2
 
+tmux new-window -t $SESSION:3 -n 'bill' $SHELL
+tmux send-keys -t $SESSION:3 "set +o history" C-m
+tmux send-keys -t $SESSION:3 "cd ${BILL_WALLET_DIR}" C-m
+
 # Start the second wallet client
-monero-wallet-rpc \
---detach \
---pidfile="${WALLETS_DIR}/bill-wallet-rpc.pid" \
-	--rpc-bind-ip 127.0.0.1 \
-	--rpc-bind-port ${BILL_WALLET_RPC_PORT} \
-	--wallet-dir "${WALLETS_DIR}" \
-	--disable-rpc-login \
-    --allow-mismatched-daemon-version
+tmux send-keys -t $SESSION:3 "monero-wallet-rpc \
+   --rpc-bind-ip 127.0.0.1 \
+   --rpc-bind-port ${BILL_WALLET_RPC_PORT} \
+   --wallet-dir ${BILL_WALLET_DIR} \
+   --disable-rpc-login \
+   --allow-mismatched-daemon-version; tmux wait-for -S fredxmr" C-m
 sleep 2
+
+tmux send-keys -t $SESSION:0 "cd ${HARNESS_CTL_DIR}" C-m
 
 ################################################################################
 # Create the wallets
@@ -170,7 +193,11 @@ done
 generate ${BILL_WALLET_PRIMARY_ADDRESS} ${ALPHA_NODE_RPC_PORT} 10
 sleep 2
 
-
 # ... any more?
+
+tmux send-keys -t $SESSION:0 "set -o history" C-m
+tmux select-window -t $SESSION:0
+
+tmux attach-session -t $SESSION
 
 echo 'harness set up'
